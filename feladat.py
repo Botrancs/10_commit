ae2ac3c9131ac59406b713ac3f166ac6f062b91d
Botrancs <banbotondlajos@turr.hu>
2023-12-13 10:35:20 +0100

    ha nem akkor addig kérdezzük amíg igent nem mond.

--- feladat.py
+++ feladat.py
@@ -4,3 +4,9 @@ if kerdes == "Igen" or "igen":
     print("Szerintem ez nem normális menj dokihoz")
 helyes= True
 kerdes_2=print("Legalább szereted a tejet?")
+if kerdes_2 == "nem" or "Nem":
+    helyes = False
+while not helyes:
+    ujra_proba=print("Na ezt próbáld meg mégegyszer.")
+    if ujra_proba == "Igen" or "igen":
+        helyes = True
