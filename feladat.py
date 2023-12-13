960664ddbdc66fc722460897eba9288535c8454c
Botrancs <banbotondlajos@turr.hu>
2023-12-13 10:34:13 +0100

    Ha igen akkor nem történik semmi

--- feladat.py
+++ feladat.py
@@ -2,4 +2,5 @@ lab_szam=input("Hány lábad van?")
 kerdes=print("Biztos", lab_szam, "lábad van?")
 if kerdes == "Igen" or "igen":
     print("Szerintem ez nem normális menj dokihoz")
+helyes= True
 kerdes_2=print("Legalább szereted a tejet?")
