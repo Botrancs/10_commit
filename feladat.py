547a246afce52e46e028358aea922acbf1d8e73b
Botrancs <banbotondlajos@turr.hu>
2023-12-13 10:28:11 +0100

    Megkérdezem, hogy szereti-e a tejet.

--- feladat.py
+++ feladat.py
@@ -2,3 +2,4 @@ lab_szam=input("Hány lábad van?")
 kerdes=print("Biztos", lab_szam, "lábad van?")
 if kerdes == "Igen" or "igen":
     print("Szerintem ez nem normális menj dokihoz")
+kerdes_2=print("Legalább szereted a tejet?")
